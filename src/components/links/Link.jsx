46ca6387b7b6b3866ba27799466d7e3e6e9d22d1
goodcodd <2858.allo.113@gmail.com>
2023-02-22 10:49:31 +0200

    22.02.2023 видалила state, використала this.props

--- src/components/links/Link.jsx
+++ src/components/links/Link.jsx
@@ -1,20 +1,9 @@
 import {Component} from "react";
-import img2 from "../../assets/img/projects/02.jpg";
 
 export default class Link extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            href: props.href,
-            text: props.text,
-            className: props.className,
-            target: props.target,
-            rel: props.rel
-        }
-    }
 
     render() {
-        const {href, text, className, rel, target} = this.state;
+        const {href, text, className, rel, target} = this.props;
         return (
             <a href={href} target={target} rel={rel}  className={className}>
                 {text}
